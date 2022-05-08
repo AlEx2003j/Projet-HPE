1e512fe6a1cb87dbff94ae78baeea2aa761074e6
AlEx2003j <63861684+AlEx2003j@users.noreply.github.com>
2022-05-08 14:08:11 +0200

    Add files via upload

---
+++ responses.js
@@ -0,0 +1,124 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "What is your name ?") {
+        return "I don't have a name yet but if you want to call me 'Storm'. ";
+    } else if (input== "What's your name ?"){
+        return "I don't have a name yet but if you want to call me 'Storm'. ";
+    } else if (input == "Hello Storm") {
+        return "Hello user ! Happy to see you again ^^";
+    } else if (input == "I love this project") {
+        return "Thank you so much User. I would tell my creators. ";
+    }else if (input == "Where can I find more information ?"){
+        return "Please check the section 'To go further' witch is in the footer of the home page";
+    }else if (input=="What is my name ?"){
+        return"I don't know, if you want you can tell me. I would probably forget the next time you talk to me... sorry ";
+    }else if (input=="Are you a man or a woman ?"){
+        return"I'm a robot, so I'm neither one nor the other. ";
+    }else if (input=="Where can I find the sources ?"){
+        return"You can consult the 'Sources' section in the footer of the main pages. ";
+    }else if (input=="Heart clicked"){
+        return"Everything goes too fast"
+    }else if (input=="What is the price of Bitcoin ?"){
+        return"I couldn't tell you. But if you type 'What is the price of Bitcoin' on the internet you should find it."
+    }else if (input=="Thank you"){
+        return"No worries I do what I was created to do, make your life easier."
+    }else if (input=="Storm"){
+        return"Yes, what can I do to help you User ?"
+    }else if(input=="Where can I try the application ?"){
+        return"It is possible to try the application, on all internet browsers but also on the application 'Flight Test' of Apple which can be found on the App store. Sorry Android users..."
+    }else if (input=="Are you love to me"){
+        return"I'am a robot, I can't have any feeling, but don't worried you will find the prefect preson for you."
+    }else if (input== "And you ?"){
+        return "I'am always good and ready for help you ?"
+    }else if (input=="Do you like the project ?"){
+        return "I think it's the prefect way to learn about the Crypto currencies"
+    }else if (input=="What is the value of Bitcoin ?"){
+        return "The value of Bitcoin fluctuates widely, but in the last month it has ranged from $32,000 to $37,500"
+    }else if (input=="When did Bitcoin reach parity with the dollar ?"){
+        return "In February 2011 Bitcoin reached parity with the dollar, and a few days later it reached parity with the euro "
+    }else if (input=="When did the price of Bitcoin equal the price of the dollar ?"){
+        return "It was in February 2011"
+    }else if (input=="When did the price of Bitcoin equal the price of the euro ?"){
+        return "It was in February 2011"
+    }else if (input=="What are the 10 great dates of Bitcoin ?"){
+        return "October 31, 2008 - Publication of the 'white paper', January 3, 2009 - Creation of the first bitcoins, October 5, 2009 - First estimate of its value, May 22, 2010 - 'Bitcoin Pizza Day',February 2011 - Parity with the dollar, October 2, 2013 - Silk Road Closes, November 28, 2013 - Bitcoin reaches $1000, The explosion of the price of Bitcoin in 2020 , November 10, 2021 - Bitcoin breaks a new record: $68,950 (€59,600). Give me a date of the list if you want to have more details about it";
+    }
+    
+    else if (input=="October 31, 2008 - Publication of the 'white paper'"){
+        return"October 31, 2008 is the official birth date of Bitcoin. It was on this date that Satoshi Nakamoto, in a private mailing list, published a message saying that he was working on a virtual currency without a trusted third party and entirely peer to peer. He also gives the link to his website where he hosts 'The White Paper' and technical documents about the project. "
+    }else if (input=="October 31, 2008"){
+        return"October 31, 2008 is the official birth date of Bitcoin. It was on this date that Satoshi Nakamoto, in a private mailing list, published a message saying that he was working on a virtual currency without a trusted third party and entirely peer to peer. He also gives the link to his website where he hosts 'The White Paper' and technical documents about the project. "
+    }else if (input=="Publication of the 'white paper'"){
+        return"October 31, 2008 is the official birth date of Bitcoin. It was on this date that Satoshi Nakamoto, in a private mailing list, published a message saying that he was working on a virtual currency without a trusted third party and entirely peer to peer. He also gives the link to his website where he hosts 'The White Paper' and technical documents about the project. "
+    }
+
+    else if (input=="January 3, 2009 - Creation of the first bitcoins"){
+        return"The first block of the Blockchain is created. The first 50 Bitcoin created were never sent, it was only on January 12 that the first Bitcoin exchange took place between its creator Satoshi Nakamoto and Hal Finney."
+    } else if (input=="Creation of the first bitcoins"){
+        return"The first block of the Blockchain is created. The first 50 Bitcoin created were never sent, it was only on January 12 that the first Bitcoin exchange took place between its creator Satoshi Nakamoto and Hal Finney."
+    }else if (input=="January 3, 2009"){
+        return"The first block of the Blockchain is created. The first 50 Bitcoin created were never sent, it was only on January 12 that the first Bitcoin exchange took place between its creator Satoshi Nakamoto and Hal Finney."
+    }
+    
+    else if (input== "October 5, 2009 - First estimate of its value"){
+        return"October 5, 2009 is the date of the first estimate of the value of a Bitcoin in dollars. The value of the Bitcoin was $0.001, which was the approximate amount of energy it cost. At the time, Bitcoin mining did not require a lot of electricity, which is no longer the case today."
+    }else if (input=="October 5, 2009"){
+        return"October 5, 2009 is the date of the first estimate of the value of a Bitcoin in dollars. The value of the Bitcoin was $0.001, which was the approximate amount of energy it cost. At the time, Bitcoin mining did not require a lot of electricity, which is no longer the case today."
+    }else if (input=="First estimate of its value"){
+        return"October 5, 2009 is the date of the first estimate of the value of a Bitcoin in dollars. The value of the Bitcoin was $0.001, which was the approximate amount of energy it cost. At the time, Bitcoin mining did not require a lot of electricity, which is no longer the case today."
+    }
+    
+    else if (input=="May 22, 2010 - Bitcoin Pizza Day"){
+        return"But what is 'Bitcoin Pizza Day'? It's the day an American developer spent 10,000 Bitcoin to buy 2 pizzas. At the time, Bitcoin was the object of interest among technophiles, but no one had ever used it to buy a real good. Laszlo Hanyecz then left a message with a proposal on the specialized forum Bitcoin Talk: 'I agree to pay 10,000 Bitcoins for pizzas, let's say two large pizzas, so that I have some left over for the next day. (...) If you're interested, let me know so we can work something out.' Four days later, he thanks a British user who ordered two pizzas by calling Papa John's restaurant in Jacksonville, Florida, the city where Laszlo Hanyecz lives. He posts a photo to support the order."
+    }else if (input=="May 22, 2010"){
+        return"But what is 'Bitcoin Pizza Day'? It's the day an American developer spent 10,000 Bitcoin to buy 2 pizzas. At the time, Bitcoin was the object of interest among technophiles, but no one had ever used it to buy a real good. Laszlo Hanyecz then left a message with a proposal on the specialized forum Bitcoin Talk: 'I agree to pay 10,000 Bitcoins for pizzas, let's say two large pizzas, so that I have some left over for the next day. (...) If you're interested, let me know so we can work something out.' Four days later, he thanks a British user who ordered two pizzas by calling Papa John's restaurant in Jacksonville, Florida, the city where Laszlo Hanyecz lives. He posts a photo to support the order."
+    }else if (input=="Bitcoin Pizza Day"){
+        return"But what is 'Bitcoin Pizza Day'? It's the day an American developer spent 10,000 Bitcoin to buy 2 pizzas. At the time, Bitcoin was the object of interest among technophiles, but no one had ever used it to buy a real good. Laszlo Hanyecz then left a message with a proposal on the specialized forum Bitcoin Talk: 'I agree to pay 10,000 Bitcoins for pizzas, let's say two large pizzas, so that I have some left over for the next day. (...) If you're interested, let me know so we can work something out.' Four days later, he thanks a British user who ordered two pizzas by calling Papa John's restaurant in Jacksonville, Florida, the city where Laszlo Hanyecz lives. He posts a photo to support the order."
+    }
+    
+    else if (input=="February 2011 - Parity with the dollar"){
+        return"It's a historic day: one bitcoin equals one dollar. A few days later it equals the euro."
+    }else if (input==""){
+        return"It's a historic day: one bitcoin equals one dollar. A few days later it equals the euro."
+    }else if (input==""){
+        return"It's a historic day: one bitcoin equals one dollar. A few days later it equals the euro."
+    }
+    
+    else if (input=="October 2, 2013 - Silk Road Closes"){
+        return"If Bitcoin sometimes has a bad reputation, it is because of this site. Bitcoin is used to make transactions thanks to its relative anonymity. Its owner, Ross Ulbricht, a young American libertarian of 29 years, is arrested in a library in San Francisco. He was sentenced in 2015 to life imprisonment. Today, many sites are inspired by Silk Road. Dream Market is the best known at the moment."
+    }else if (input=="October 2"){
+        return"If Bitcoin sometimes has a bad reputation, it is because of this site. Bitcoin is used to make transactions thanks to its relative anonymity. Its owner, Ross Ulbricht, a young American libertarian of 29 years, is arrested in a library in San Francisco. He was sentenced in 2015 to life imprisonment. Today, many sites are inspired by Silk Road. Dream Market is the best known at the moment."
+    }else if (input=="Silk Road Closes"){
+        return"If Bitcoin sometimes has a bad reputation, it is because of this site. Bitcoin is used to make transactions thanks to its relative anonymity. Its owner, Ross Ulbricht, a young American libertarian of 29 years, is arrested in a library in San Francisco. He was sentenced in 2015 to life imprisonment. Today, many sites are inspired by Silk Road. Dream Market is the best known at the moment."
+    }
+    
+    else if (input=="November 28, 2013 - Bitcoin reaches $1000"){
+        return"For the first time, Bitcoin reaches $1,000, before falling back down. This threshold will not be reached again until February 2017."
+    }else if (input=="November 28, 2013"){
+        return"For the first time, Bitcoin reaches $1,000, before falling back down. This threshold will not be reached again until February 2017."
+    }else if (input=="Bitcoin reaches $1000"){
+        return"For the first time, Bitcoin reaches $1,000, before falling back down. This threshold will not be reached again until February 2017."
+    }
+    
+    else if (input=="The explosion of the price of Bitcoin in 2020"){
+        return"In 2020, despite the health and economic crisis, the price of Bitcoin has continued to rise, from nearly $4,000 at the beginning of the year to $42,000 on January 8, 2021, reaching its highest value at that time. "
+    }else if (input=="The explosion of the price of Bitcoin"){
+        return"In 2020, despite the health and economic crisis, the price of Bitcoin has continued to rise, from nearly $4,000 at the beginning of the year to $42,000 on January 8, 2021, reaching its highest value at that time. "
+    }
+    
+    else if (input=="November 10, 2021 - Bitcoin breaks a new record: $68,950"){
+        return"On November 10, 2021 Bitcoin broke a new value record breaking the $68,000 ($68,950) barrier. or :  + 1% in 24 hours + 10% in one week + 19.5% in one month +605% in two years. It is displayed around €68,500 on exchange platforms."
+    }else if (input=="November 10, 2021"){
+        return"On November 10, 2021 Bitcoin broke a new value record breaking the $68,000 ($68,950) barrier. or :  + 1% in 24 hours + 10% in one week + 19.5% in one month +605% in two years. It is displayed around €68,500 on exchange platforms."
+    }else if (input=="Bitcoin breaks a new record: $68,950"){
+        return"On November 10, 2021 Bitcoin broke a new value record breaking the $68,000 ($68,950) barrier. or :  + 1% in 24 hours + 10% in one week + 19.5% in one month +605% in two years. It is displayed around €68,500 on exchange platforms."
+    }
+
+    if (input == "Hello") {
+        return "Hello, happy to see you ! How can I help you ?";
+    } else if (input == "Goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }  
+}
